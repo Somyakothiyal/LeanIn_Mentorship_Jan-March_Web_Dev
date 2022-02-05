5d60dd815c2053d45d3cd15a5759cbb9cfd78acc
somya kothiyal <91784227+Somyakothiyal@users.noreply.github.com>
2022-02-06 00:05:35 +0530

    Update App.js

--- Milestone 1/src/App.js
+++ Milestone 1/src/App.js
@@ -1,42 +1,4 @@
-// // import TextInput from "./component/TextInput";
-// // import Welcome from "./component/Welcome";
-// import React,{ useState } from "react";
-// import AddUser from "./component/User/AddUser";
-// import UserList from "./component/User/UserList";
-// // import Button from "./component/UI/Button";
-// // import Card from "./component/UI/Card";
-// function App(){
-//   const [UserList, setUserList]=useState([]);
-//   const onAddUserHandler = (userName, userAge) =>{
-//     setUserList((prevState)=>{
-//       return [...prevState, {name: userName,age: userAge,id:Math.random().toString()}]} 
-//     )
-//   }
-// return (
-//   <div>
-//     <AddUser onAddUser = {onAddUserHandler}/>
-//     <UserList list= {UserList}/>
-//     )
-//   </div>
-// );
-//   };
-// const App=()=> {
-//   return (
-//     <div>
-//       //   <TextInput/>
-//       // <Welcome/>  
-//       <Card/> 
-//       <Button/>
-      
 
-// //     </div>
-// //   );
-// // };
-
-// export default App;
-
-//import TextInput from "./components/TextInput";
-//import Welcome from "./components/Welcome";
 import React, { useState } from "react";
 import AddUser from "./component/User/AddUser";
 import UserList from "./component/User/UserList";
