b9ec61a437c67c88ac60b67bdf03e9625f38fb14
somya kothiyal <91784227+Somyakothiyal@users.noreply.github.com>
2022-02-05 23:55:44 +0530

    Add files via upload

---
+++ Milestone 1/src/component/UI/Card.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+import "./Card.css";
+// import classes from './Button';
+
+// const Card = (props) => {
+//     return <div className={'${classes.card} ${props.className}'}>{props.children}</div>;
+
+// };
+// export default Card;
+
+const Card=(props)=>{
+    return(<div className="card">
+    {props.children}
+  </div>)
+}
+
+export default Card;
