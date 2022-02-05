e3b5dae0e727ae29855c619618de85b6f9051d6c
somya kothiyal <91784227+Somyakothiyal@users.noreply.github.com>
2022-02-05 23:48:20 +0530

    Update Button.js

--- Milestone 1/src/component/UI/Button.js
+++ Milestone 1/src/component/UI/Button.js
@@ -1,19 +1,4 @@
-// import react from "react";
 
-// import classes from './Button.module.css';
-
-// const Button = (props) =>{
-//     return (
-//         <Button
-//         className={classes.Button}
-//         type={props.type|| 'Button'}
-//         onClick={props.onClick}
-// >
-//         {props.children}
-//         </Button>
-//     );
-//     };
-// export default Button;
 const Button=(props)=>{return(
     <div>
         <button type={props.type}>
