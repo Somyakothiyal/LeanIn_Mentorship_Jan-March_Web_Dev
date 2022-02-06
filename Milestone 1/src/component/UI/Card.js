be41a96851c01461d3e53dfbd395c7878e3e5568
somya kothiyal <91784227+Somyakothiyal@users.noreply.github.com>
2022-02-06 20:01:13 +0530

    Update Card.js

--- Milestone 1/src/component/UI/Card.js
+++ Milestone 1/src/component/UI/Card.js
@@ -1,14 +1,7 @@
-// import React from 'react';
 
 
 import "./Card.css";
-// import classes from './Button';
 
-// const Card = (props) => {
-//     return <div className={'${classes.card} ${props.className}'}>{props.children}</div>;
-
-// };
-// export default Card;
 
 const Card=(props)=>{
     return(<div className="card">
